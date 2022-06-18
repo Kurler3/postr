eeb1424cbe103b1bd137a477e0bddec4cc4821d0
Kurler3 <miguelbento2000@gmail.com>
2022-06-18 18:15:32 +0100

    Fixed mongodb uri showing

--- backend/mongodb/mongoose.ts
+++ backend/mongodb/mongoose.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 export default async function connectDb() {
     try {
-        await mongoose.connect(process.env.MONGO_DB_KEY!);
+        await mongoose.connect(process.env.MONGO_DB_URI!);
         console.log("Mongoose connected");
         
     } catch (error) {
